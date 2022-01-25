f2c41b7f34a6f05437eedc78a75c258a8439e61d
joseph roby <josephroby99@gmail.com>
2022-01-25 13:57:43 +0530

    -created login page.

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import LoginPage from "./pages/LoginPage";
 import PostPage from "./pages/PostPage";
 import PostDetails from "./pages/PostDetails";
 import ProfilePage from "./pages/ProfilePage";
-import "./styles/App.css";
 
 function App() {
   return (
@@ -13,6 +12,8 @@ function App() {
         <Route path="postPage" element={<PostPage />} />
         <Route path="postDetails" element={<PostDetails />} />
         <Route path="profilePage" element={<ProfilePage />} />
+
+
       </Routes>
     </div>
   );
--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -1,16 +1,102 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import "../styles/LoginPage.css";
 
-function LoginPage() {
+export default function Login() {
+  const [formData, setFormData] = React.useState({ email: "", password: "" })
+  const [item, setItems] = React.useState([])
+  const [formErrors, setFormErrors] = React.useState({});
+  const history = useNavigate();
+
+
+  React.useEffect(function () {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then((response) => response.json())
+      .then(data => setItems(data))
+  }, [])
+
+
+  function handleChange(event) {
+    setFormData(prevFormData => {
+      return {
+        ...prevFormData,
+        [event.target.name]: event.target.value
+      };
+    });
+  }
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    setFormErrors(validate(formData));
+  };
+
+  const validate = (values) => {
+    const errors = {};
+    const userid = formData.email;
+    const passid = formData.password;
+
+
+
+    item.map((items) => {
+      var paswd = items.username;
+      var num = 123;
+      paswd += num;
+      if (items.email !== userid && paswd !== passid) {
+        errors.username = "Invalid username or password";
+      }
+      else if (items.email === userid && paswd !== passid) {
+        errors.username = "Invalid username or passsword";
+      }
+      else if (items.email !== userid && paswd === passid) {
+        errors.username = "Invalid username or passsword";
+      }
+      else {
+        history("postDetails");
+      }
+    });
+    return errors;
+  };
   return (
-    <div>
-      <h1>This is Login Page</h1>
-      <Link to="postPage">Click to view Post page.</Link>
-      <br />
-      <Link to="postDetails">Click here to view Post Details.</Link>
-      <br />
-      <Link to="profilePage">Click here to view Profile Page.</Link>
+    <div className="container">
+      <h1>META</h1>
+
+      <form onSubmit={handleSubmit} classname="form-input">
+        <div></div>
+        <p>{formErrors.username}</p>
+        <label>
+          Username:
+
+          <input
+
+            className='user-input'
+            type="email"
+            name="email"
+            placeholder="Your email id"
+            onChange={handleChange}
+            value={formData.email}
+          />
+        </label>
+        <br></br>
+        <p>{formErrors.password}</p>
+        <label>
+          Password:
+
+          <input
+            className='user-password'
+            type="password"
+            name="password"
+            placeholder="Enter your password"
+            onChange={handleChange}
+            value={formData.password}
+          />
+        </label>
+        <br></br>
+        <button type="submit" className="login-button">
+          Log In
+        </button>
+      </form>
     </div>
+
   );
-}
-export default LoginPage;
+};
+
+
